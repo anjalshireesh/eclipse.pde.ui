842b4551e31bde730e20bf971f8a7c2d621479a3
cvs2svn <platform-releng-dev@eclipse.org>
2006-12-03 20:51:48 +0000

    This commit was manufactured by cvs2svn to create branch 'NewModels'.
    Cherrypick from master 2006-12-03 20:51:46 UTC Wassim Melhem <wmelhem> 'removed deprecations':
        ui/org.eclipse.pde.ui/src/org/eclipse/pde/internal/ui/commands/CommandComposerDialog.java
        ui/org.eclipse.pde.ui/src/org/eclipse/pde/internal/ui/commands/CommandComposerPart.java
        ui/org.eclipse.pde.ui/src/org/eclipse/pde/internal/ui/commands/CommandCopyFilter.java
        ui/org.eclipse.pde.ui/src/org/eclipse/pde/internal/ui/commands/CommandDetails.java
        ui/org.eclipse.pde.ui/src/org/eclipse/pde/internal/ui/commands/CommandList.java
        ui/org.eclipse.pde.ui/src/org/eclipse/pde/internal/ui/commands/CommandTreeContentProvider.java
        ui/org.eclipse.pde.ui/src/org/eclipse/pde/internal/ui/commands/CommandView.java
        ui/org.eclipse.pde.ui/src/org/eclipse/pde/internal/ui/commands/QueryByObjectSelection.java
        ui/org.eclipse.pde.ui/src/org/eclipse/pde/internal/ui/commands/QueryByTag.java
        ui/org.eclipse.pde.ui/src/org/eclipse/pde/internal/ui/commands/QueryControl.java
        ui/org.eclipse.pde.ui/src/org/eclipse/pde/internal/ui/commands/TagManager.java
        ui/org.eclipse.pde.ui/src/org/eclipse/pde/internal/ui/editor/contentassist/ManifestContentAssistProcessor.java
        ui/org.eclipse.pde.ui/src/org/eclipse/pde/internal/ui/editor/text/ManifestConfiguration.java
        ui/org.eclipse.pde.ui/src/org/eclipse/pde/internal/ui/launcher/LaunchPluginValidator.java
        ui/org.eclipse.pde.ui/src/org/eclipse/pde/internal/ui/wizards/plugin/NewLibraryPluginCreationPage.java
        ui/org.eclipse.pde.ui/src/org/eclipse/pde/internal/ui/wizards/plugin/NewProjectCreationOperation.java
        ui/org.eclipse.pde.ui/src/org/eclipse/pde/internal/ui/wizards/plugin/NewProjectCreationPage.java
        ui/org.eclipse.pde.ui/src/org/eclipse/pde/ui/launcher/AbstractPDELaunchConfigurationTabGroup.java
        ui/org.eclipse.pde.ui/src/org/eclipse/pde/ui/launcher/EclipseLaunchShortcut.java
        ui/org.eclipse.pde.ui/src/org/eclipse/pde/ui/launcher/JUnitWorkbenchLaunchShortcut.java
        ui/org.eclipse.pde.ui/src/org/eclipse/pde/ui/templates/AbstractTemplateSection.java

---
+++ ui/org.eclipse.pde.ui/src/org/eclipse/pde/internal/ui/commands/CommandComposerDialog.java
@@ -0,0 +1,83 @@
+/*******************************************************************************
+ * Copyright (c) 2006 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.pde.internal.ui.commands;
+
+import org.eclipse.core.commands.Command;
+import org.eclipse.core.commands.ParameterizedCommand;
+import org.eclipse.jface.dialogs.IDialogConstants;
+import org.eclipse.jface.viewers.ISelectionChangedListener;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.viewers.SelectionChangedEvent;
+import org.eclipse.pde.internal.ui.PDEUIMessages;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.forms.IManagedForm;
+import org.eclipse.ui.forms.widgets.ScrolledForm;
+import org.eclipse.ui.internal.provisional.forms.FormDialog;
+
+public class CommandComposerDialog extends FormDialog {
+	
+	private CommandComposerPart fCCP;
+	private ParameterizedCommand fPC;
+	private Button fOKButton;
+	
+	public CommandComposerDialog(Shell parentShell, int filterType, ParameterizedCommand preselectedCommand) {
+		super(parentShell);
+		setShellStyle(SWT.MODELESS | SWT.SHELL_TRIM | SWT.BORDER);
+		fCCP = new CommandComposerPart();
+		fCCP.setFilterType(filterType);
+		fCCP.setPresetCommand(preselectedCommand);
+	}
+	
+	protected Control createContents(Composite parent) {
+		Control control = super.createContents(parent);
+		fOKButton = getButton(IDialogConstants.OK_ID);
+		fOKButton.setEnabled(fCCP.getPresetCommand() != null);
+		return control;
+	}
+	
+	protected void createFormContent(IManagedForm mform) {
+		ScrolledForm form = mform.getForm();
+		initializeDialogUnits(form);
+		fCCP.createCC(form, mform.getToolkit(), new ISelectionChangedListener() {
+			public void selectionChanged(SelectionChangedEvent event) {
+				if (fOKButton != null) {
+					Object obj = event.getSelection();
+					fOKButton.setEnabled(obj instanceof IStructuredSelection &&
+							((IStructuredSelection)obj).getFirstElement() instanceof Command);
+				}
+			}
+		});
+		applyDialogFont(form);
+	}
+	
+	protected void configureShell(Shell newShell) {
+		newShell.setText(PDEUIMessages.CommandSerializerPart_name);
+		super.configureShell(newShell);
+	}
+	
+	public void okPressed() {
+		fPC = fCCP.getParameterizedCommand();
+		super.okPressed();
+	}
+	
+	public boolean close() {
+		fCCP.dispose();
+		return super.close();
+	}
+	
+	public ParameterizedCommand getCommand() {
+		return fPC;
+	}
+}
---
+++ ui/org.eclipse.pde.ui/src/org/eclipse/pde/ui/launcher/AbstractPDELaunchConfigurationTabGroup.java
@@ -0,0 +1,133 @@
+/*******************************************************************************
+ * Copyright (c) 2005, 2006 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.pde.ui.launcher;
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.Preferences;
+import org.eclipse.debug.core.ILaunchConfiguration;
+import org.eclipse.debug.core.ILaunchConfigurationWorkingCopy;
+import org.eclipse.debug.ui.AbstractLaunchConfigurationTabGroup;
+import org.eclipse.debug.ui.ILaunchConfigurationTab;
+import org.eclipse.jdt.launching.IJavaLaunchConfigurationConstants;
+import org.eclipse.pde.internal.core.ICoreConstants;
+import org.eclipse.pde.internal.core.PDECore;
+import org.eclipse.pde.internal.core.TargetPlatform;
+import org.eclipse.pde.internal.ui.launcher.LaunchPluginValidator;
+import org.eclipse.swt.custom.BusyIndicator;
+import org.eclipse.swt.widgets.Display;
+
+/**
+ * An abstract class subclassed by the Eclipse application and JUnit Plug-in launch
+ * configuration tab groups.
+ * <p>
+ * This class is not intended to be subclassed by clients.
+ * </p>
+ * @since 3.3
+ */
+public abstract class AbstractPDELaunchConfigurationTabGroup extends
+		AbstractLaunchConfigurationTabGroup {
+
+	/**
+	 * The tab group delegates to all tabs in the group.
+	 * Prior to the delegation, it migrates all pre-3.2 launch configurations
+	 * to make them 3.2-compliant.
+	 * 
+	 * @see org.eclipse.debug.ui.AbstractLaunchConfigurationTabGroup#initializeFrom(org.eclipse.debug.core.ILaunchConfiguration)
+	 */
+	public void initializeFrom(ILaunchConfiguration configuration) {
+		final ILaunchConfiguration config = configuration;
+		final ILaunchConfigurationTab[] tabs = getTabs();
+		BusyIndicator.showWhile(Display.getCurrent(), new Runnable() {
+			public void run() {
+				try {
+					if (config instanceof ILaunchConfigurationWorkingCopy) {
+						checkBackwardCompatibility(
+							(ILaunchConfigurationWorkingCopy) config);
+					}
+				} catch (CoreException e) {
+				}
+				for (int i = 0; i < tabs.length; i++) {
+					tabs[i].initializeFrom(config);
+				}
+			}
+		});
+	}
+	
+	/**
+	 * Checks if the launch configuration is 3.2-compliant and migrates it if it's not.
+	 * 
+	 * @param wc 
+	 * 			the launch configuration to be migrated if it's not 3.2-compliant
+	 * @throws CoreException
+	 * 			a CoreException is thrown if there was an error retrieving launch 
+	 * 			configuration attributes
+	 */
+	private void checkBackwardCompatibility(ILaunchConfigurationWorkingCopy wc) throws CoreException {
+		String id = wc.getAttribute(
+						IJavaLaunchConfigurationConstants.ATTR_SOURCE_PATH_PROVIDER,
+						(String) null);
+		if (id == null) {
+			wc.setAttribute(
+				IJavaLaunchConfigurationConstants.ATTR_SOURCE_PATH_PROVIDER,
+				PDESourcePathProvider.ID);
+		}
+		
+		String value = wc.getAttribute("vmargs", (String)null); //$NON-NLS-1$
+		if (value != null) {
+			wc.setAttribute("vmargs", (String)null); //$NON-NLS-1$
+			wc.setAttribute(IJavaLaunchConfigurationConstants.ATTR_VM_ARGUMENTS, value);
+		}
+		
+		value = wc.getAttribute("progargs", (String)null); //$NON-NLS-1$
+		if (value != null) {
+			wc.setAttribute("progargs", (String)null); //$NON-NLS-1$
+			wc.setAttribute(IJavaLaunchConfigurationConstants.ATTR_PROGRAM_ARGUMENTS, value);
+		}
+		
+		value = wc.getAttribute(IPDELauncherConstants.LOCATION + "0", (String)null); //$NON-NLS-1$
+		if (value != null) {
+			wc.setAttribute(IPDELauncherConstants.LOCATION + "0", (String)null); //$NON-NLS-1$
+			wc.setAttribute(IPDELauncherConstants.LOCATION, value);			
+		}
+		
+		LaunchPluginValidator.checkBackwardCompatibility(wc, false);	
+		wc.doSave();
+	}
+
+	/**
+	 * Delegates to all tabs to set defaults.
+	 * It then sets program and VM arguments based on values on the 
+	 * <b>Plug-in Development > Target Platform > Launching Arguments</b> preference page.
+	 * 
+	 * @see org.eclipse.debug.ui.AbstractLaunchConfigurationTabGroup#setDefaults(org.eclipse.debug.core.ILaunchConfigurationWorkingCopy)
+	 */
+	public void setDefaults(ILaunchConfigurationWorkingCopy configuration) {
+		super.setDefaults(configuration);
+		if (TargetPlatform.usesNewApplicationModel())
+			configuration.setAttribute("pde.version", "3.3"); //$NON-NLS-1$ //$NON-NLS-2$
+		else if (TargetPlatform.getTargetVersion() >= 3.2)
+			configuration.setAttribute("pde.version", "3.2a"); //$NON-NLS-1$ //$NON-NLS-2$
+
+		configuration.setAttribute(
+			IJavaLaunchConfigurationConstants.ATTR_SOURCE_PATH_PROVIDER,
+			PDESourcePathProvider.ID); 
+		
+		// Set Program/VM arguments with preference values
+		Preferences preferences = PDECore.getDefault().getPluginPreferences();
+		String programArgs = preferences.getString(ICoreConstants.PROGRAM_ARGS);
+		if (programArgs.length() > 0)
+			configuration.setAttribute(IJavaLaunchConfigurationConstants.ATTR_PROGRAM_ARGUMENTS, programArgs);
+		String vmArgs = preferences.getString(ICoreConstants.VM_ARGS);
+		if (vmArgs.length() > 0)
+			configuration.setAttribute(IJavaLaunchConfigurationConstants.ATTR_VM_ARGUMENTS, vmArgs);
+	}
+
+}
